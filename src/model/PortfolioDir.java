e9687786214fddb01e5518ffc8ce4dabf4fb0f94
97sachi <156970519+97sachi@users.noreply.github.com>
2024-03-13 23:37:59 -0400

    Update PortfolioDir.java

--- src/model/PortfolioDir.java
+++ src/model/PortfolioDir.java
@@ -1,20 +1,61 @@
 package model;
 
-import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Map;
 
+/**
+ * The PortfolioDir interface represents a directory of portfolios.
+ * It provides methods for managing portfolios, such as adding portfolio, getting list of portfolio.
+ * as well as retrieving portfolio composition and total value of portfolio in portfolio directory.
+ */
 public interface PortfolioDir {
+
+  /**
+   * Adds a new portfolio to the portfolio directory.
+   * @param newBuilder The builder object used to construct the new portfolio.
+   */
   void addPortfolio(PortfolioImpl.PortfolioBuilder newBuilder);
 
+  /**
+   * Retrieves the list of names of all portfolios in the directory.
+   * @return the list of portfolio names.
+   */
   ArrayList<String> getListOfPortfoliosName();
 
+  /**
+   * Retrieves the composition of the portfolio at the specified index in the directory.
+   * @param input the index of the portfolio.
+   * @return map containing composition of the portfolio (share name and quantity in a portfolio.)
+   */
   Map<String, Integer> portfolioComposition(int input);
 
+  /**
+   * Retrieves the number of portfolios in the directory.
+   * @return the number of portfolios in the directory list.
+   */
   int getSize();
 
+  /**
+   * portfolio value method gets the total value of portfolio present in portfolio directory at.
+   * specified index for a given date
+   * @param input the index of the portfolio in portfolio directory.
+   * @param day the day of date for which total value is to be found.
+   * @param month the month of date for which total value is to be found.
+   * @param year the year of date for which total value is to be found.
+   * @return the total value of portfolio.
+   */
   double portfolioValue(int input, int day, int month, int year);
 
+  /**
+   * Checks if the directory is empty (contains no portfolios).
+   * @return true if the directory is empty, or else false.
+   */
   boolean isEmpty();
+
+  /**
+   * checks if a portfolio name exists or not in the portfolio directory.
+   * @param portfolioName the portfolio name that is to be checked in the directory.
+   * @return true if portfolio name is present or returns false.
+   */
   boolean portfolioNameExists(String portfolioName);
 }
