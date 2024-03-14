9d09653da5e259446351d3859e0638e5e6072b5f
97sachi <156970519+97sachi@users.noreply.github.com>
2024-03-13 23:39:41 -0400

    Update StockInterface.java

--- src/model/StockInterface.java
+++ src/model/StockInterface.java
@@ -1,5 +1,14 @@
 package model;
 
+/**
+ * StockInterface class is used to implement methods for fetching the stock price data.
+ */
 public interface StockInterface {
+
+  /**
+   * return price method is used to get the price of a particular stock for entered date.
+   * @param date is the date for which user wants to get the stock price.
+   * @return the price value of the stock on a particular date.
+   */
   double returnPrice(String date);
 }
