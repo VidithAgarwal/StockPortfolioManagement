8eea2075b560827f0d4b68c502e96705e54e4a79
97sachi <156970519+97sachi@users.noreply.github.com>
2024-03-13 23:37:19 -0400

    Update FileHandler.java

--- src/model/FileHandler.java
+++ src/model/FileHandler.java
@@ -10,7 +10,17 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
+/**
+ * This class FileHandler as methods for loading the data from files, that are created when.
+ * api is called for a stock data. It also saves the data to a file, which is fetched from api.
+ */
 public class FileHandler {
+
+  /**
+   * Loads data from a file located at the specified file path.
+   * @param filePath The path to the file to be loaded.
+   * @return A list of string arrays representing the lines of data read from the file.
+   */
   public List<String[]> load(String filePath) {
     List<String[]> lines = new ArrayList<>();
     try (BufferedReader reader = new BufferedReader(new FileReader(filePath))) {
@@ -25,6 +35,12 @@ public class FileHandler {
     return lines;
   }
 
+  /**
+   * Saves data to a file located at the specified path.
+   * @param path The path to the file where data will be saved.
+   * @param historicalData A map representing the historical data to be saved, with keys as
+   *                       stock names and values as historical prices.
+   */
   void save(String path, Map<String, Double> historicalData) {
     File file = new File(path);
     try (BufferedWriter writer = new BufferedWriter(new FileWriter(file))) {
