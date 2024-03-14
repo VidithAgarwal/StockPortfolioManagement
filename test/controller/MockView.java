9b22604bf86f43cdfefc71ad20586d9c6002669b
97sachi <156970519+97sachi@users.noreply.github.com>
2024-03-13 23:41:53 -0400

    Update MockView.java

--- test/controller/MockView.java
+++ test/controller/MockView.java
@@ -1,53 +1,85 @@
 package controller;
 
-import java.io.PrintStream;
 import java.util.ArrayList;
 import java.util.Map;
 
 import view.IView;
 
+/**
+ * The MockView class is a mock implementation of the IView interface.
+ * It is used for testing purposes.
+ */
 public class MockView implements IView {
 
+  /**
+   * The StringBuilder object used to store printed output.
+   */
   private final StringBuilder printedOutput = new StringBuilder();
 
+  /**
+   * Retrieves the printed output.
+   * @return The printed output.
+   */
   public StringBuilder getPrintedOutput() {
     return printedOutput;
   }
 
   @Override
   public void showPrimaryMenu() {
+    // has no input and no output.
   }
 
   @Override
   public void showSecondaryMenu() {
+    // has no input and no output.
   }
 
+  /**
+   * Displays the composition of the portfolio.
+   * @param composition The map containing the composition of the portfolio.
+   *                   (stock names and quantities).
+   */
   @Override
   public void showComposition(Map<String, Integer> composition) {
-    // this we can remove
     for (Map.Entry<String, Integer> entry : composition.entrySet()) {
       printedOutput.append(entry.getKey()).append(" ").append(entry.getValue()).append("\n");
     }
   }
 
+  /**
+   * Displays the total value of the portfolio.
+   * @param value The total value of the portfolio.
+   */
   @Override
   public void showTotalValue(double value) {
     printedOutput.append("The total value of portfolio is: $").append(value).append("\n");
   }
 
+  /**
+   * Displays the list of portfolios.
+   * @param listOfPortfolios The list of portfolios.
+   */
   @Override
   public void showListOfPortfolios(ArrayList<String> listOfPortfolios) {
-    // this we can remove
     for (int i = 0; i < listOfPortfolios.size(); i++) {
       printedOutput.append(i).append(listOfPortfolios.get(i)).append("\n");
     }
   }
 
+  /**
+   * Displays an error message.
+   * @param error The error message to display.
+   */
   @Override
   public void displayError(String error) {
     printedOutput.append(error).append("\n");
   }
 
+
+  /**
+   * Prints a message.
+   * @param message The message to print.
+   */
   @Override
   public void print(String message) {
     printedOutput.append(message).append("\n");
