4757c157e7955b39570e62fb3c4f7ec93663ec54
97sachi <156970519+97sachi@users.noreply.github.com>
2024-03-14 17:21:15 -0400

    Update StockController.java

--- src/controller/StockController.java
+++ src/controller/StockController.java
@@ -1,7 +1,18 @@
 package controller;
 
 /**
- * interface of controller, StockController.
+ * This interface represents a stock controller.
+ * Controllers implementing this interface are responsible for delegating input from user to.
+ * model and view.
  */
 public interface StockController {
+
+  /**
+   * Initiates the main functionality of the application, handling the flow of the user interface.
+   * It prompts the user with the appropriate menu based on the state of the portfolio directory.
+   * If the portfolio directory is empty, it displays the primary menu.
+   * otherwise, it displays the secondary menu.
+   * The method continues looping until the user chooses to exit the application.
+   */
+  void execute();
 }
