d0387eee766d6ab5f2d9a79eebbb1e672221bc5f
97sachi <156970519+97sachi@users.noreply.github.com>
2024-03-25 18:28:52 -0400

    Update IViewImpl.java

--- src/view/IViewImpl.java
+++ src/view/IViewImpl.java
@@ -1,9 +1,9 @@
 package view;
 
 import java.io.PrintStream;
-import java.util.ArrayList;
 import java.util.Map;
 import java.util.TreeMap;
+
 
 /**
  * Implementation of the IView interface providing methods to display menus, portfolio composition,
@@ -31,6 +31,9 @@ public class IViewImpl implements IView {
     this.err = err;
   }
 
+  /**
+   * Displays the primary menu options, when any portfolio is not created, at start.
+   */
   @Override
   public void showPrimaryMenu() {
     out.println("Main Menu");
@@ -40,6 +43,9 @@ public class IViewImpl implements IView {
     out.println("4. Exit");
   }
 
+  /**
+   * Displays the secondary menu options, which is shown once at least one portfolio is created.
+   */
   @Override
   public void showSecondaryMenu() {
     out.println("Main Menu");
@@ -52,9 +58,15 @@ public class IViewImpl implements IView {
     out.println("7. Sell stock");
     out.println("8. Get cost basis");
     out.println("9. Get Stock Statistics");
-    out.println("10. Exit");
+    out.println("10. Get Portfolio Performance, for Flexible Portfolio");
+    out.println("11. Exit");
   }
 
+
+  /**
+   * Displays the composition of a portfolio.
+   * @param composition Map containing stock names and quantities.
+   */
   @Override
   public void showComposition(Map<String, Integer> composition) {
     out.println("Composition of Portfolio");
@@ -64,6 +76,9 @@ public class IViewImpl implements IView {
     }
   }
 
+  /**
+   * Displays the option to choose the type of portfolio, flexible or inflexible.
+   */
   @Override
   public void choosePortfolioType() {
     out.println("1. Inflexible Portfolio");
@@ -72,6 +87,9 @@ public class IViewImpl implements IView {
   }
 
 
+  /**
+   * Displays the options for stock statistics for stock trend statistics.
+   */
   @Override
   public void showStockStat() {
     out.println("1. Get gain or lose on date for a stock");
@@ -83,11 +101,20 @@ public class IViewImpl implements IView {
     out.println("7. Exit");
 
   }
+
+  /**
+   * Displays the total value of a portfolio.
+   * @param value Total value of the portfolio to be printed.
+   */
   @Override
   public void showTotalValue(double value) {
     out.println("The total value of portfolio is: $" + value);
   }
 
+  /**
+   * Displays the X-day moving average value.
+   * @param value The X-day moving average value that is to be printed.
+   */
   @Override
   public void showXDayMovingAvg(double value) {
     out.println("The X-day moving average is: $" + value);
@@ -105,13 +132,15 @@ public class IViewImpl implements IView {
 //    }
 //  }
 
+
+  /**
+   * Displays the list of existing portfolios.
+   * @param listOfPortfolios Map containing portfolio names and their types.
+   */
   @Override
   public void showListOfPortfolios(Map<String, String> listOfPortfolios) {
-    //can print this message using print that list of flexible portfolio, list of inflexible portfolio
-    // or can have a parameter as string type that gives portfolio type, but it will be changed in existing code.
     out.println("The list of existing portfolios:");
     out.println("No. of Portfolio\t Portfolio Name");
-
 
     int index = 0;
     for (Map.Entry<String, String> entry : listOfPortfolios.entrySet()) {
@@ -123,18 +152,35 @@ public class IViewImpl implements IView {
   }
 
 
+  /**
+   * Displays an error message.
+   * @param error Error message to be displayed.
+   */
   @Override
   public void displayError(String error) {
     err.println(error);
   }
 
+  /**
+   * Prints a message.
+   * @param message Message to be printed.
+   */
   @Override
   public void print(String message) {
     out.println(message);
   }
 
+  /**
+   * Displays a bar graph using *.
+   *
+   * @param scale           Scaling factor for the bar graph.
+   * @param data Data for the bar graph that has represents the date and number of * to be printed.
+   * @param stockOrPortfolio Name of the stock or portfolio.
+   * @param startDate       Start date from which graph is represented.
+   * @param endDate         End date which is the end date until which graph is represented.
+   */
   @Override
-  public void barGraph(int scale, Map<String, Integer> data, String stockOrPortfolio, String startDate, String endDate) {
+  public void barGraph(int scale, TreeMap<String, Integer> data, String stockOrPortfolio, String startDate, String endDate) {
     out.println("Performance of portfolio " + stockOrPortfolio + " from " + startDate + " to " + endDate + "\n");
     for (Map.Entry<String, Integer> entry : data.entrySet()) {
       String timestamp = entry.getKey();
@@ -145,11 +191,16 @@ public class IViewImpl implements IView {
       }
       out.println(timestamp + ": " + asterisks);
     }
-    out.print("\nScale: * = \n"+ scale);
+    out.print("\nScale: * = "+ scale +"\n");
   }
 
+  /**
+   * Prints entries of a TreeMap in proper format.
+   * @param treeMap TreeMap whose entries which are to be printed.
+   */
   @Override
   public void printTreeMapEntries(TreeMap<String, String> treeMap) {
+    out.print("The crossover opportunities are:\n");
     for (Map.Entry<String, String> entry : treeMap.entrySet()) {
       out.println(entry.getKey() + " : " + entry.getValue());
     }
