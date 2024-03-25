aacd5ef1cb08d645d4bf672539aa71218aa52703
97sachi <156970519+97sachi@users.noreply.github.com>
2024-03-25 13:42:07 -0400

    Update AbstractPortfolio.java

--- src/model/AbstractPortfolio.java
+++ src/model/AbstractPortfolio.java
@@ -7,23 +7,52 @@ import java.util.Map;
 
 import controller.StockData;
 
+/**
+ * An abstract class implementing common functionality for flexible and inflexible portfolio.
+ */
 abstract class AbstractPortfolio implements Portfolio {
+
+  /**
+   * The name of the portfolio.
+   */
   protected final String portfolioName;
 
+  /**
+   * constructs an AbstractPortfolio object with the specified name.
+   * @param portfolioName name of the portfolio.
+   */
   protected AbstractPortfolio(String portfolioName) {
     this.portfolioName = portfolioName;
   }
 
+  /**
+   * retrieves name of the portfolio.
+   * @return name of the portfolio.
+   */
   @Override
   public String getName() {
     return portfolioName;
   }
 
+  /**
+   * this method retrieves closing price of a stock on a specified date.
+   * @param ticker ticker symbol of the stock.
+   * @param api  StockData object used to fetch historical data.
+   * @param date date for which the closing price is to be retrieved.
+   * @return closing price of the stock on the specified date.
+   */
   protected double getClosingPriceOnDate(String ticker, StockData api, String date) {
     Map<String, ArrayList<Double>> priceData = api.fetchHistoricalData(ticker);
     return priceData.get(date).get(1);
   }
 
+  /**
+   * this method computes total value of the portfolio on a specified date.
+   * @param date date for which the portfolio value is to be computed.
+   * @param composition composition of the portfolio (stock ticker symbols and quantities).
+   * @param api StockData object used to fetch historical data.
+   * @return total value of the portfolio on the specified date.
+   */
   protected double computeValue(String date, Map<String, Integer> composition, StockData api) {
     double totalValue = 0;
     for (Map.Entry<String, Integer> entry : composition.entrySet()) {
@@ -39,6 +68,11 @@ abstract class AbstractPortfolio implements Portfolio {
     return totalValue;
   }
 
+  /**
+   * this method validates if the share name exists in the stocks.csv file.
+   * @param shareName The name of the share to be validated.
+   * @return ticker symbol of the share if found, otherwise null.
+   */
   static String validateStockName(String shareName) {
     FileHandler fileHandler = new FileHandler();
     List<String[]> lines = fileHandler.load("stocks.csv");
@@ -58,6 +92,11 @@ abstract class AbstractPortfolio implements Portfolio {
     return null; // Return null if no matching ticker symbol or company name is found
   }
 
+  /**
+   * this method creates a deep copy of a map.
+   * @param map1 the map to be copied.
+   * @return new map containing same key-value pairs as input map.
+   */
   protected Map<String, Integer> deepCopy(Map<String, Integer> map1) {
     Map<String, Integer> map2 = new HashMap<>();
     for (Map.Entry<String, Integer> entry : map1.entrySet()) {
