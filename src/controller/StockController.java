4c7b1d5fc8659661f73625fb690d8a8a0e761e79
97sachi <156970519+97sachi@users.noreply.github.com>
2024-03-26 16:14:50 -0400

    Update StockController.java

--- src/controller/StockController.java
+++ src/controller/StockController.java
@@ -1,15 +1,15 @@
 package controller;
 
 /**
-<<<<<<< HEAD
+ <<<<<<< HEAD
  * This class represents a controller with a single action method. It's designed to handle specific
  * requests within an application. The go() method is the primary entry point for the
  * action this controller performs.
-=======
+ =======
  * This interface represents a stock controller.
  * Controllers implementing this interface are responsible for delegating input from user to.
  * model and view.
->>>>>>> 006209f2c9f83e0fc6312ad3d5388e091f7a6634
+ >>>>>>> 006209f2c9f83e0fc6312ad3d5388e091f7a6634
  */
 public interface StockController {
 
