10fad658cdd72a04bc53f06556cb4b80d2316a7f
97sachi <156970519+97sachi@users.noreply.github.com>
2024-03-27 17:40:40 -0400

    Update FlexiblePortfolioBuySellHelper.java

--- src/controller/FlexiblePortfolioBuySellHelper.java
+++ src/controller/FlexiblePortfolioBuySellHelper.java
@@ -6,11 +6,29 @@ import java.util.Scanner;
 import model.PortfolioDir;
 import view.IView;
 
-class FlexiblePortfolioBuySellHelper extends AbsHelperController{
+/**
+ * FlexiblePortfolioBuySellHelper class provides helper methods for buying & selling stocks.
+ * in flexible portfolios.
+ * It extends AbsHelperController class, it uses the methods of view and model.
+ */
+class FlexiblePortfolioBuySellHelper extends AbsHelperController {
+
+
+  /**
+   * constructs FlexiblePortfolioBuySellHelper object with specified view, model, and scanner.
+   * @param view view component for user interaction.
+   * @param model  model component for managing portfolios.
+   * @param scan scanner object for user input.
+   */
   FlexiblePortfolioBuySellHelper(IView view, PortfolioDir model, Scanner scan) {
     super(view, model, scan);
   }
 
+  /**
+   * this method allows user to buy stocks for a flexible portfolio.
+   * Prompts user for portfolio choice, share name, quantity, and purchase date.
+   * Calls model's buyStock method to handle purchase, and view to displays the messages.
+   */
   void buyStock() {
     int choice = inputPortfolioChoice();
 
@@ -28,13 +46,18 @@ class FlexiblePortfolioBuySellHelper extends AbsHelperController{
     }
   }
 
+  /**
+   * this method allows user to sell stocks from a flexible portfolio.
+   * Prompts user for portfolio choice, share name, quantity, and sale date.
+   * Calls model's sellStock method to handle the sale, and view to displays the messages.
+   */
   void sellStock() {
     int choice = inputPortfolioChoice();
 
     view.print("Enter the name of the share or ticker symbol: ");
     String shareName = scan.nextLine();
-    int quantity = inputPositiveInteger("Enter the quantity of " + shareName + " you want to " +
-            "sell:");
+    int quantity = inputPositiveInteger("Enter the quantity of " + shareName + " you want to "
+            + "sell:");
     int[] date = inputDate("Enter the date of your sale");
     LocalDate sellDate = LocalDate.of(date[2], date[1],date[0]);
 
