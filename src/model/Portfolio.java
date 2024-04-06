d486aa30df3e9920d1490b31c094c95f5ab6511e
97sachi <156970519+97sachi@users.noreply.github.com>
2024-04-06 13:39:11 -0400

    Update Portfolio.java

--- src/model/Portfolio.java
+++ src/model/Portfolio.java
@@ -88,5 +88,13 @@ public interface Portfolio {
    */
   void load(List<String[]> lines, IStockData api);
 
+  /**
+   * this method executes dollar-cost averaging investment strategy.
+   * for provided schedule & current date.
+   * using given stock data API to fetch historical prices.
+   * @param today     current date.
+   * @param schedule  buying schedule defining the investment strategy.
+   * @param api       stock data API to fetch historical prices.
+   */
   void dollarCostAverage(LocalDate today, BuyingStrategy schedule, StockData api);
 }
