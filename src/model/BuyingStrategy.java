85d0fe9061adfb750ae70cf28ff877ed3a840b39
97sachi <156970519+97sachi@users.noreply.github.com>
2024-04-06 13:36:30 -0400

    Update BuyingStrategy.java

--- src/model/BuyingStrategy.java
+++ src/model/BuyingStrategy.java
@@ -3,19 +3,54 @@ package model;
 import java.time.LocalDate;
 import java.util.Map;
 
+/**
+ * Interface representing a buying strategy for investments .
+ * This class represents ability to invest a specific amount in an existing flexible portfolio on.
+ * a specific date by specifying the weights of how that money should be invested in each stock.
+ * inside that portfolio.
+ */
 public interface BuyingStrategy {
 
+  /**
+   * this method returns amount of money to be invested in each transaction.
+   * @return amount of money to invest.
+   */
   double getAmount();
 
+  /**
+   * the method returns frequency of transactions in days.
+   * @return frequency of transactions in days.
+   */
   int getFrequencyDays();
 
+  /**
+   * the method returns the start date of the buying strategy.
+   * @return start date of the buying strategy.
+   */
   LocalDate getStartDate();
 
+  /**
+   * the method returns the end date of the buying strategy.
+   * @return end date of the buying strategy.
+   */
   LocalDate getEndDate();
 
+  /**
+   * the method returns transaction fee associated with each transaction.
+   * @return transaction fee.
+   */
   double getTransactionFee();
 
+  /**
+   * the method returns date when the strategy was last executed.
+   * @return last execution date.
+   */
   LocalDate getLastRunDate();
 
+  /**
+   * the method returns a map representing buying list, where keys are symbols of assets to buy.
+   * and values are corresponding amounts to invest in each asset.
+   * @return buying list as a map.
+   */
   Map<String, Double> getBuyingList();
 }
