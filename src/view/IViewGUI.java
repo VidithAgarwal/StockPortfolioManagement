3a84404ae275dac70b9905c301af7915c893807f
97sachi <156970519+97sachi@users.noreply.github.com>
2024-04-11 08:33:23 -0400

    Update IViewGUI.java

--- src/view/IViewGUI.java
+++ src/view/IViewGUI.java
@@ -1,14 +1,22 @@
 package view;
 
-import java.util.Map;
 
 import controller.Features;
 
+/**
+ * This IViewGUI interface defines methods for interacting with.
+ * graphical user interface (GUI) components.
+ */
 public interface IViewGUI {
 
+  /**
+   * this method add feature sets Features instance for GUI and connects view with controller.
+   * by defining all the call back functions on based of action to perform when used in view.
+   * @param features is Features instance to be set.
+   *              
+   */
   void addFeatures(Features features);
 
-  String getPortfolioName();
 
   /**
    * this method displays an error message.
@@ -22,14 +30,6 @@ public interface IViewGUI {
    */
   void print(String message);
 
-  String getPositiveInput();
 
-  int[] inputDate();
-
-  int selectPortfolio(Map<String, String> listOfPortfolios);
-
-  String getPath();
-
-  String getShareName();
 
 }
